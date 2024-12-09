b7cb93c152b1e56c2cd8234563c024268d7ec801
shivsehr140 <shivsehr140@gmail.com>
2024-12-09 23:17:08 +0530

    verison 1.2

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
->>>>>>> f22c1dbf199e5655d7f3aaec7568aa6df32929d5
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-})
-=======
 });
->>>>>>> f22c1dbf199e5655d7f3aaec7568aa6df32929d5
